e2afb30d36533a9ac01be62c53c679d092d752d6
fewensa <osuni@protonmail.com>
2023-07-04 02:44:59 +0000

    Add scripts

---
+++ scripts/airnode-deployer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
+
+docker run -it --rm \
+  -v "${WORK_PATH}:/app/config" \
+  api3/airnode-deployer:0.11.2 \
+  $@
+
+
---
+++ scripts/remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+
+
+${BIN_PATH}/airnode-deployer.sh remove-with-receipt
+
+
+
