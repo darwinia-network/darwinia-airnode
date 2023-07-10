87fd997ff9b13c79ad22ba2e993daf2024036f49
fewensa <37804932+fewensa@users.noreply.github.com>
2023-07-10 12:03:08 +0800

    Refactor Airnode (#9)
    * Refactor airnode
    
    * Improve again
    
    * Update readme
    
    * Change to LF
    
    * chmod +x
    
    * Add Darwinia&Crab
    
    * split api key
    
    ---------
    
    Co-authored-by: Guantong <i@gt.email>

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 ## How to use this config
 
 1. Replace ${YOUR_API_KEY} with your key from Infura or Alchemy
+
+
+## Deploy
+
+### Deploy Cloud
+
+```
+./scripts/deploy-cloud.sh
+```
+
+### Remove Cloud
+
+```
+./scripts/remove-cloud.sh
+```
+
+### Deploy Local
+
+```
+./scripts/deploy-local.sh
+```
---
+++ scripts/deploy-local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
+
+docker rm quick-start-container-airnode || true
+
+docker run \
+  --volume "${WORK_PATH}:/app/config" \
+  --name quick-start-container-airnode \
+  --network host \
+  api3/airnode-client-dev:0b7d89eb582d63aa299216f3cc28d82cf7071981
