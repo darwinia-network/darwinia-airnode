2a7aabc9109bbcf31131f4488ae58e100587cade
Guantong <i@gt.email>
2023-07-11 14:42:01 +0800

    template

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -3,10 +3,12 @@
 BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
 WORK_PATH=${BIN_PATH}/../
 
+${BIN_PATH}/generate-config.sh
+
 docker rm quick-start-container-airnode || true
 
 docker run \
   --volume "${WORK_PATH}:/app/config" \
   --name quick-start-container-airnode \
   --network host \
-  api3/airnode-client-dev:0b7d89eb582d63aa299216f3cc28d82cf7071981
+  api3/airnode-client:0.11.2
