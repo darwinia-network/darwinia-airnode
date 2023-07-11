35123385fb46b8fed744dd6ad20a483be858f193
Guantong <i@gt.email>
2023-07-11 13:37:51 +0800

    Prefix env

--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -17,15 +17,17 @@ fi
 
 set -a; . ${SECRETS_ENV}; set +a
 
-if [ ${CLOUD_PROVIDER} == "aws" ]; then
+if [[ ${CLOUD_PROVIDER} == "aws" ]]; then
   jq -r 'del(.nodeSettings.cloudProvider.projectId)' ${CONFIG_EXAMPLE_FILE} > ${CONFIG_TARGET_FILE}
+  else
+  cp ${CONFIG_EXAMPLE_FILE} ${CONFIG_TARGET_FILE}
 fi
 
 env | while read -r line
 do
   KEY=$(echo $line | cut -d '=' -f 1)
   VALUE=$(echo $line | cut -d '=' -f 2-)
-  sed -i "s#\${${KEY}}#${VALUE}#g" ${CONFIG_TARGET_FILE}
+  sed -i "s|\${${KEY}}|${VALUE}|g" ${CONFIG_TARGET_FILE}
 done
 
 sed -i -E "s/\\$\{.*}//g" ${CONFIG_TARGET_FILE}
