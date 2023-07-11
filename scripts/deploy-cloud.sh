8e4f9b9e16788bdd8d002807172582f8d9057348
fewensa <osuni@protonmail.com>
2023-07-11 03:07:01 +0000

    rename config json

--- scripts/deploy-cloud.sh
+++ scripts/deploy-cloud.sh
@@ -3,9 +3,25 @@
 
 
 BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
 
 
-${BIN_PATH}/airnode-deployer.sh deploy
+CONFIG_FILE=${WORK_PATH}/config.json
+SECRETS_ENV=${WORK_PATH}/secrets.env
+
+if [ ! -f ${SECRETS_ENV} ]; then
+  echo 'missing secrets.env file'
+  exit 1
+fi
+
+. ${SECRETS_ENV}
+
+if [ ${CLOUD_PROVIDER} == "aws" ]; then
+  jq -r 'del(.nodeSettings.cloudProvider.projectId)' ${CONFIG_FILE} > ${WORK_PATH}/config.local.json
+fi
+
+
+#${BIN_PATH}/airnode-deployer.sh deploy
 
 
 
