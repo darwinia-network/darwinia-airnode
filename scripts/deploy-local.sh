ce02bcf4a0e6324f08cacdd84844f180e5d772e8
Guantong <i@gt.email>
2023-11-02 11:37:54 +0800

    update script

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -11,9 +11,10 @@ CONTAINER_NAME=${1:-quick-start-container-airnode}
 
 docker stop ${CONTAINER_NAME} || true
 docker rm ${CONTAINER_NAME} || true
+# You can specify port by replacing `--network=host` with `-p=<YOUR_PORT>:3000`
 docker run -dit \
   --restart=always \
   --volume="${WORK_PATH}:/app/config" \
   --name=${CONTAINER_NAME} \
-  -p=3000:3000 \
+  --network=host \
   api3/airnode-client:0.12.0
